56d6743a680fa6b02b2fbb6084d2a31d6a063b51
nhabedding <lengocha2502@gmail.com>
2022-03-22 23:02:41 +0700

    bai5

--- unit3/bai4.php
+++ unit3/bai4.php
@@ -7,7 +7,7 @@ function checkPalindrome($string)
     // Sử dụng hàm đảo ngược chuỗi strrev() hoặc explod chuỗi rồi áp dụng bào 4.2 đảo ngược mảng
     if ($string == strrev($string)) {
         echo $string . " là chuỗi palindrome.";
-    } else {
+    } else  {
         echo $string . " không là chuỗi palindrome.";
     }
 }
