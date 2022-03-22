47f2b4984fa2276327480085920712ce0a78f7f6
nhabedding <lengocha2502@gmail.com>
2022-03-22 22:04:53 +0700

    1

--- unit3/bai3.php
+++ unit3/bai3.php
@@ -1,13 +1,15 @@
 <?php 
 
-$name=" lê thị   ngọc hà ";
+$name=" lê thị   ngọC hà ";
 echo "chuỗi chưa được chuẩn hóa:<br>";
 echo "$name<br>";
-// viết hoa chữ cái đầu tiên
-$name= ucwords($name);
-// bỏ khoang trắng di
-$name=trim($name);
 
+$name= ucwords($name);
+// viết hoa chữ cái đầu tiên
+$name = mb_strtolower($name);
+//đổi chữ hoa thành thường
+$name=trim($name);
+// bỏ khoang trắng di
 echo "chuỗi được chuẩn hóa:<br>";
 echo $name;
 
